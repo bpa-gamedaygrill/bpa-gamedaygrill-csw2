c7ff6ff280af949c32eb5cc65420b4bda70e9bd9
Rohit Karthik <rohitismaster@gmail.com>
2023-10-13 18:55:09 -0400

    Created new custom AnimateOnScroll client component, which animates the children elements placed inside of it on scroll.

---
+++ app/components/AOS/AnimateOnScroll.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { ReactNode } from 'react'
+import { motion, useInView } from "framer-motion";
+
+interface AnimateOnScrollInterface {
+  refElement: React.RefObject<HTMLElement>;
+  children?: ReactNode;
+  customStyling ?: string;
+  duration: number;
+  originalScale: number;
+  once:boolean;
+}
+
+const AnimateOnScroll: React.FC<AnimateOnScrollInterface> = ({ refElement, children, customStyling, duration, originalScale, once }) => {
+  const isInView = useInView(refElement, { once: once });
+  
+  return (
+    <>
+      { isInView && <motion.div
+        className={`${customStyling}`}
+        initial={{ scale: originalScale, opacity: 0, filter: 'blur(3px)' }}
+        animate={{ scale: 1, opacity: 1, filter: 'blur(0px)' }}
+        transition={{ duration: duration, ease: 'easeInOut' }}
+        >
+        {children}
+        </motion.div>
+      }
+  </>
+  )
+}
+
+export default AnimateOnScroll;
