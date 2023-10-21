0fe7e4a4a5e244b7ef0d2be0f8e3f0e0cef42fc6
Rohit Karthik <rohitismaster@gmail.com>
2023-10-20 22:18:09 -0400

    Added sign up page with sign up functionality

--- app/signup/components/Form.tsx
+++ app/signup/components/Form.tsx
@@ -1,11 +1,96 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import { parseCookies, setCookie, destroyCookie } from 'nookies';
 
 const Form = () => {
+  const cookies = parseCookies()
+
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [fullName, setFullName] = useState<string>("");
+  const [fullNameError, setFullNameError] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log("test", { cookies })
+    if (cookies.token) {
+      window.location.replace("/");
+    }
+  }, [])
+
+  const updateFullName = (e: any) => {
+    setFullNameError(() => false);
+    setFullName(() => e.target.value);
+  }
+  const updateEmail = (e: any) => {
+    setEmailError(() => false);
+    setEmail(() => e.target.value);
+  }
+  const updatePassword = (e: any) => {
+    setPasswordError(() => false);
+    setPassword(() => e.target.value);
+  }
+
+  const handleFormSubmit = () => {
+    let errorCount: number = 0;
+    setIsLoading(true);
+    if (!fullName.trim()) {
+      setFullNameError(() => true);
+      errorCount++
+      setIsLoading(false);
+    }
+    if (!email.trim()) {
+      setEmailError(() => true);
+      errorCount++
+      setIsLoading(false);
+    }
+    if (!password.trim()) {
+      setPasswordError(() => true);
+      errorCount++
+      setIsLoading(false);
+    }
+    if (errorCount > 0) {
+      return
+    }
+
+    axios.post('/api/user/create', {
+      fullName: fullName,
+      email: email,
+      password: password
+    })
+    .then(response => {
+      setCookie(null, 'token', response.data.token)
+      console.log("SUCCESSFULLY SIGNED UP!");
+      console.log("RESPONSE:", response)
+    })
+    .catch(error => {
+        console.error("An error has occured. Please check DB and Server logs.");
+    })
+    .finally(() => {
+        setIsLoading(() => false);
+    })
+
+  }
   return (
   <>
+      <div className="w-full flex flex-col items-start font-medium text-md justify-start text-neutral-700 gap-3">
+        <p className={`${ fullNameError && "text-primary-red" }`}>Full Name*</p>
+        <input type='text' onChange={updateFullName} value={fullName} placeholder='' className="px-4 py-2.5 bg-white border-[1px] border-neutral-200 rounded-md focus:outline-none focus:border-neutral-300 w-full text-neutral-700 font-medium" />
+      </div>
+      <div className="w-full flex flex-col items-start font-medium text-md justify-start text-neutral-700 gap-3">
+        <p className={`${ emailError && "text-primary-red" }`}>Email*</p>
+        <input type='text' onChange={updateEmail} value={email} placeholder='' className="px-4 py-2.5 bg-white border-[1px] border-neutral-200 rounded-md focus:outline-none focus:border-neutral-300 w-full text-neutral-700 font-medium" />
+      </div>
+      <div className="w-full flex flex-col items-start font-medium text-md justify-start text-neutral-700 gap-3">
+        <p className={`${ passwordError && "text-primary-red" }`}>Password*</p>
+        <input type='text' onChange={updatePassword} value={password} placeholder='' className="px-4 py-2.5 bg-white border-[1px] border-neutral-200 rounded-md focus:outline-none focus:border-neutral-300 w-full text-neutral-700 font-medium" />
+      </div>
 
+      <button className={`${ isLoading ? "bg-red-600/70 cursor-auto" : "bg-primary-red cursor-pointer hover:bg-red-700" } text-white text-sm font-semibold py-3.5 px-4 rounded-md`} onClick={handleFormSubmit}>Sign Up</button>
   </>
   )
 }
--- app/signup/components/SignupFormContent.tsx
+++ app/signup/components/SignupFormContent.tsx
@@ -7,11 +7,13 @@ const SignupFormContent = () => {
   return (
     <>
       <div className="w-full flex flex-col items-center max-w-[475px] mr-auto ml-auto">
-        <div className="w-full flex flex-col items-start gap-1 mb-16">
+        <div className="w-full flex flex-col items-start gap-1 mb-12">
           <h1 className="text-[2.1rem] font-semibold tracking-tight">Get started today</h1>
           <p className="text-[16px] font-medium text-neutral-600 tracking-tight dark:text-neutral-500">Sign up to recieve perks, discounts, cash-back, and more!</p>
         </div>
+        <div className="flex flex-col gap-7 w-full">
         <Form />
+        </div>
       </div>
     </>
   )
