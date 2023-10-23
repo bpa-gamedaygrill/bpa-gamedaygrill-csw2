9f7107da72d437a9e393aa17fa39ef6f49c11415
Rohit Karthik <rohitismaster@gmail.com>
2023-10-22 21:34:45 -0400

    Initialized redis and redis client and cloud redis instance

---
+++ libs/redis/redis.ts
@@ -0,0 +1,10 @@
+import { Redis } from "ioredis";
+
+const getRedisUrl = () => {
+  if (process.env.REDIS_URL) {
+    return process.env.REDIS_URL;
+  }
+  throw new Error("REDIST_URL not defined");
+}
+
+export const redis = new Redis(getRedisUrl());
