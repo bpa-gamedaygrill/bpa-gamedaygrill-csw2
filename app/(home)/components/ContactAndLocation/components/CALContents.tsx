119a233b0431beed4f90ab954c5b3d94068433c8
Rohit Karthik <rohitismaster@gmail.com>
2023-11-07 13:44:56 -0500

    Updated Google Location

--- app/(home)/components/ContactAndLocation/components/CALContents.tsx
+++ app/(home)/components/ContactAndLocation/components/CALContents.tsx
@@ -16,7 +16,7 @@ const CALContents = () => {
 
         {/* Right Side of the Hero Section (Where the IMG will go) */}
         <section className="w-full flex items-center justify-end h-full md:max-w-[50%] relative">
-          <div className="relative w-full md:w-[80%] h-[350px] cursor-pointer group" onClick={() => window.open("https://www.google.com/maps/dir//39.6666344,-75.6651434/@39.5991805,-75.7123939,11.36z?entry=ttu")}>
+          <div className="relative w-full md:w-[80%] h-[350px] cursor-pointer group" onClick={() => window.open("https://www.google.com/maps/dir//Delaware+19709/@39.5386097,-75.6932644,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c709e71a7463cf:0x1bd6c9891d3e1577!2m2!1d-75.667356!2d39.5392979?entry=ttu")}>
           <div className="absolute w-full h-full bg-black/0 rounded-xl group-hover:bg-black/10 z-10"/>
           <div className="z-20 absolute py-2 px-3 bg-white top-3 left-3 rounded-sm group-hover:opacity-80">
               <p className="font-medium text-sm md:text-[0.7rem] opacity-50 group-hover:opacity-70">2023 GDG Street, DE</p>
