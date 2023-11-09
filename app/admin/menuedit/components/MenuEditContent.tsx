1cdcfb28882ff77f6c83b747f592e032be83f5ab
Rohit Karthik <rohitismaster@gmail.com>
2023-11-09 14:06:22 -0500

    Updated menu edit admin page to dynamically fetch data

--- app/admin/menuedit/components/MenuEditContent.tsx
+++ app/admin/menuedit/components/MenuEditContent.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 import Link from 'next/link'
 
+import db from "../../../../libs/prisma/db";
+
 import MenuEditItem from './MenuEditItem'
 
-const MenuEditContent = () => {
+const MenuEditContent = async() => {
+  const menuItems = await db.menuItem.findMany();
+  console.log(`MENU ITEMS FROM EDIT PAGE: ${JSON.stringify(menuItems)}`)
   return (
   <>
       <section className="w-full bg-white border-[1px] border-neutral-300 py-6 px-8 rounded-lg">
@@ -14,11 +18,14 @@ const MenuEditContent = () => {
               <p className="font-medium tracking-tight opacity-50">Go Back</p>
           </Link>
         </div>
-        <div className="mt-14 w-full flex items-start justify-between flex-wrap gap-5">
-          <MenuEditItem />
-          <MenuEditItem />
-          <MenuEditItem />
-          <MenuEditItem />
+        <div className="mt-14 grid grid-cols-1 sm:grid-cols-2  gap-7 mb-28 justify-center items-center">
+      {
+      menuItems && menuItems.map((item) => (
+        <>
+            <MenuEditItem key={item.id} imageUrl={item.imageUrl} itemName={item.itemName} itemDesc={item.itemDescription as string} itemPrice={item.itemPrice} />
+        </>
+      ))
+      }
         </div>
       </section>
   </>
--- app/admin/menuedit/components/MenuEditItem.tsx
+++ app/admin/menuedit/components/MenuEditItem.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const MenuEditItem = async() => {
+import Image from "next/image";
+
+interface MenuEditItemInterface {
+  imageUrl: string;
+  itemName: string;
+  itemDesc: string;
+  itemPrice: string;
+}
+
+const MenuEditItem: React.FC<MenuEditItemInterface> = async({ imageUrl, itemName, itemDesc, itemPrice }) => {
   return (
   <>
-      <div>
-        test
+      <div className="w-full py-5 px-7 bg-white group rounded-lg border-[1px] gap-7 border-neutral-300 flex items-center justify-between">
+        <div className="relative w-full h-full min-h-[125px] max-w-[175px] max-h-[125px]">
+          <Image 
+          src={imageUrl}
+          alt="Invalid Image URl"
+          layout="fill"
+          className="rounded-md object-cover"
+          /> 
+        </div>
+        <div className="w-full h-full gap-3 flex flex-col items-start justify-between">
+          <h1 className="text-xl opacity-70 group-hover:opacity-90 transition-all duration-300 font-semibold tracking-tight">{itemName}</h1>
+          <p className="text-sm font-medium opacity-60 tracking-tight">{itemDesc}</p>
+          <p className="text-md font-medium text-primary-red">${itemPrice}</p>
+        </div>
       </div>
   </>
   )
