3053e99f4be33af4de70341599d42027e2cbd837
Rohit Karthik <rohit@Rohits-MBP.fios-router.home>
2023-11-29 19:06:39 -0500

    Test commit to fix vercel errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata = {
   description: 'Game Day Grill, submission for 2023-2024 (435) BPA Web Design Competition',
 };
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function RootLayout({
