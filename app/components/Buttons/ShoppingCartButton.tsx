5fc2526f30dd31484ca237076e3abfacc0bda80c
Rohit Karthik <rohitismaster@gmail.com>
2023-11-30 13:11:14 -0500

    Added dot to cart btn

--- app/components/Buttons/ShoppingCartButton.tsx
+++ app/components/Buttons/ShoppingCartButton.tsx
@@ -6,6 +6,9 @@ import { ShoppingBag } from 'react-feather';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { close, open } from "../../redux/features/cartModalSlice";
 
+// Dexie utilities
+import { useLiveQuery } from 'dexie-react-hooks';
+import dexie from '../../../libs/dexie';
 
 export interface HamburgerMenuButtonInterface {
   buttonDimensions: number;
@@ -16,13 +19,17 @@ const ShoppingCartButton: React.FC<HamburgerMenuButtonInterface> = ({ customStyl
   const dispatch = useAppDispatch();
   const menuState = useAppSelector((state) => state.hamburgerMenuReducer.opened);
 
+  const cartItems = useLiveQuery(
+    () => dexie.cartItems.toArray()
+  );
   return (
     <>
       <button style={{ width: `${buttonDimensions}px`, height: `${buttonDimensions}px` }} className={`w-[${buttonDimensions}px] h-[${buttonDimensions}px] min-w-[40px] min-h-[40px] bg-white border-2 border-[#EDEDED] rounded-full hover:bg-neutral-50 flex items-center justify-center transition-all relative duration-150 ease-in-out ${customStyles}`} onClick={() => dispatch(open())}>
         <ShoppingBag
         color='#B5B5B5'
-        size={buttonDimensions - 25}
+        size={buttonDimensions - 23}
         />
+      <div className={`transition-all duration-200 ${ (cartItems?.length==0) ? "opacity-0" : "cart-btn-animate" } absolute top-[30%] translate-y-[-50%] right-[6px] w-[10px] h-[10px] bg-primary-red rounded-full`} />
       </button>
     </>
   )
