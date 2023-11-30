65fbb8bc82ff4b2511145eeaf9655fbb4253cfe1
Rohit Karthik <rohitismaster@gmail.com>
2023-11-30 13:30:39 -0500

    Increased cart menu size

--- app/components/NavigationMenus/SlidingCartMenu.tsx
+++ app/components/NavigationMenus/SlidingCartMenu.tsx
@@ -19,7 +19,7 @@ const SlidingCartMenu: React.FC = () => {
   return (
     <>
       <section className={`hidden md:block top-0 left-0 minimal-scrollbar fixed w-full h-full bg-black/40 backdrop-blur-[2px] z-[150] ${ cartState===false && "pointer-events-none opacity-0 delay-300 transition-all duration-100 ease-in-out" }`}>
-        <nav className={`h-full flex flex-col items-start w-full bg-white max-w-[330px] px-5 py-6 ${cartState===false ? "right-[-100%]" : "right-[0%]"} absolute top-0 transition-all duration-300 ease-in-out overflow-y-auto`}>
+        <nav className={`h-full flex flex-col items-start w-full bg-white max-w-[350px] px-5 py-6 ${cartState===false ? "right-[-100%]" : "right-[0%]"} absolute top-0 transition-all duration-300 ease-in-out overflow-y-auto`}>
           <div className={`flex w-full flex-row-reverse items-center justify-between`}>
             <MenuLogo />
             <button className="w-[45px] h-[45px] bg-white border-2 border-light-gray hover:bg-neutral-100 flex items-center justify-center rounded-full p-1" onClick={() => dispatch(close())}>
